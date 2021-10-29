aff1fe3f4dc2f602ab1665af96d9e05c26a3f4fa
nanunzoey <nanunzoey@gmail.com>
2021-10-29 21:23:00 +0900

    Update main.py
    Removed unnecessary comments

--- main.py
+++ main.py
@@ -4,15 +4,6 @@ from so import get_so_jobs
 from remoteok import get_re_jobs
 from save import save_to_file
 
-"""
-These are the URLs that will give you remote jobs for the word 'python'
-
-https://stackoverflow.com/jobs?r=true&q=python
-https://weworkremotely.com/remote-jobs/search?term=python
-https://remoteok.io/remote-dev+python-jobs
-
-Good luck!
-"""
 
 app = Flask("gigforgeeks")
 
